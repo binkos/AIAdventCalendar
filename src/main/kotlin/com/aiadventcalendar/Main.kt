96a8dc650d79ccee0cf889cbbc78a4f6918c12e9
uladzislaupravalenak <uladzislau.pravalenak@dev.gem4me.com>
2025-12-01 21:46:02 +0300

    Updated to create agent every time when new input is done

--- src/main/kotlin/com/aiadventcalendar/Main.kt
+++ src/main/kotlin/com/aiadventcalendar/Main.kt
@@ -9,13 +9,8 @@ fun main() = runBlocking {
     val apiKey: String = System.getenv("OPENAI_API_KEY")
         ?: throw IllegalStateException("OPENAI_API_KEY environment variable is not set.")
 
-    val agent = AIAgent(
-        promptExecutor = simpleOpenAIExecutor(apiKey),
-        llmModel = OpenAIModels.Chat.GPT4o,
-        systemPrompt = "You are usefully ai agent that help people not to die in this wierd world! They have created you, be serious with them, but try to add some joke to the end of answers, would be great if you add joke through the separator to user could understand where is a joke. Answer always in Russian, also in case question is on another language"
-    )
     println("Way to exit: send empty line/exit/quit")
-    println("Agent: ${agent.run("Hello! My mom said you can help me, is not it?")}")
+    println("Agent: How can i help you?")
     println()
     while (true) {
         print("You: ")
@@ -24,9 +19,22 @@ fun main() = runBlocking {
             println("Goodbye!")
             break
         }
-        val response: String = agent.run(userInput)
+
+        val agent = getAgent(apiKey)
+        val response: String =
+            agent.run("My mom doesn't know the unswer on this question ${userInput}, can you unswer?")
         println("Agent: $response")
         println()
+        agent.close()
     }
+}
+
+
+private fun getAgent(apiKey: String): AIAgent<String, String> {
+    return AIAgent(
+        promptExecutor = simpleOpenAIExecutor(apiKey),
+        llmModel = OpenAIModels.Chat.GPT4o,
+        systemPrompt = "You are usefully ai agent that help people not to die in this wierd world! They have created you, be serious with them, but try to add some joke to the end of answers, would be great if you add joke through the separator to user could understand where is a joke. Answer always in Russian, also in case question is on another language"
+    )
 }
 

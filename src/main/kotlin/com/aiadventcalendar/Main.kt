70c4158d63d90ee3212a070ebbd6ef759d165f3a
uladzislaupravalenak <uladzislau.pravalenak@dev.gem4me.com>
2025-12-01 18:31:42 +0300

    day 1 config gradle+koog, implemented while true loop with agent requests

---
+++ src/main/kotlin/com/aiadventcalendar/Main.kt
@@ -0,0 +1,32 @@
+package com.aiadventcalendar
+
+import ai.koog.agents.core.agent.AIAgent
+import ai.koog.prompt.executor.clients.openai.OpenAIModels
+import ai.koog.prompt.executor.llms.all.simpleOpenAIExecutor
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val apiKey: String = System.getenv("OPENAI_API_KEY")
+        ?: throw IllegalStateException("OPENAI_API_KEY environment variable is not set.")
+
+    val agent = AIAgent(
+        promptExecutor = simpleOpenAIExecutor(apiKey),
+        llmModel = OpenAIModels.Chat.GPT4o,
+        systemPrompt = "You are usefully ai agent that help people not to die in this wierd world! They have created you, be serious with them, but try to add some joke to the end of answers, would be great if you add joke through the separator to user could understand where is a joke. Answer always in Russian, also in case question is on another language"
+    )
+    println("Way to exit: send empty line/exit/quit")
+    println("Agent: ${agent.run("Hello! My mom said you can help me, is not it?")}")
+    println()
+    while (true) {
+        print("You: ")
+        val userInput: String? = readLine()
+        if (userInput.isNullOrBlank() || userInput.lowercase() == "exit" || userInput.lowercase() == "quit") {
+            println("Goodbye!")
+            break
+        }
+        val response: String = agent.run(userInput)
+        println("Agent: $response")
+        println()
+    }
+}
+
